da6ac71fa96cc0f320e0e53da4c9d6b3050814fa
Nam Nguyễn <nvnact@gmail.com>
2021-10-23 15:31:07 +0700

    23/10

--- Java/SpringProject/src/main/java/com/nvn/repository/IAttendenceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nvn.repository;
-
-import com.nvn.entities.Attendence;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IAttendenceRepository extends JpaRepository<Attendence, String> {
-}
