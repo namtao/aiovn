37bc968c71084f76af91310f7ab5e1a6c480c8b9
namtao <48555338+namtao@users.noreply.github.com>
2021-11-25 08:49:26 +0700

    Delete Python directory

--- Python/Applications/batch/tree.bat
+++
@@ -1,3 +0,0 @@
-set /p mypath=">>"
-cd /d "%mypath%"
-tree /a >> tree.txt
--- Python/Applications/utils/string_utils.py
+++
@@ -1,7 +0,0 @@
-def format_str(strA):
-    specialcharacters = ['\\', '/', ':', '*', '?', '"', ',', '<', '>', '|']
-    for i in strA:
-        if(i in specialcharacters):
-            strA = strA.replace(i, "")
-
-    return str(strA)
