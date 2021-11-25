1e8b639513c30b7a2e887c0486561e0a74caaafb
Nam Nguyễn <nvnact@gmail.com>
2021-11-25 09:21:58 +0700

    Revert "Auto-committed on  9:15:49,65 - 25/11/2021"
    This reverts commit 7688025f61936ec599d4a006ce8e9a7848d2f5cd.

---
+++ Python/Applications/auto/spam.py
@@ -0,0 +1,24 @@
+import pyautogui
+import pyperclip
+import random
+import time
+
+
+print("Tool Spam 1.0")
+msg = input("Nhập nội dung cần spam: ").split(" || ")
+n = int(input("Nhập số lần Spam: "))
+m = float(input("Nhập thời gian delay: "))
+
+print("Chuẩn bị")
+# Đếm ngược 5 giây
+for i in range(5, 0, -1):
+    print(i, end="...", flush='True')
+    time.sleep(1)
+print("Bắt đầu")
+
+# SPAM
+for i in range(n):
+    pyperclip.copy(random.choice(msg))
+    pyautogui.hotkey("ctrl", "v")
+    pyautogui.press("enter")
+    time.sleep(m)  # Delay
