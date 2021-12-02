eb1d592d4041786d787ce2b55ff2ac3858c51ffd
Nam Nguyễn <nvnact@gmail.com>
2021-12-02 08:44:26 +0700

    Auto-committed on  8:44:26,21 - 02/12/2021

--- Python/Applications/test.py
+++ Python/Applications/test.py
@@ -1,2 +1,2 @@
-import tensorflow as tf
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+# import tensorflow as tf
+# print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
