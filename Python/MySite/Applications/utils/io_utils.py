bc481b3aa5838f17425a9410c856432f4bfbda0e
Nam Nguyễn <vannam.pxb@gmail.com>
2022-02-16 15:38:11 +0700

    Auto-committed on 15:38:10,78 - 16/02/2022

--- Python/MySite/Applications/utils/io_utils.py
+++ Python/MySite/Applications/utils/io_utils.py
@@ -1,7 +1,4 @@
-from inspect import getfile
 import os
-from openpyxl import Workbook
-import shutil
 
 
 def get_files(folderPath, fileFormat):
@@ -82,20 +79,22 @@ def after_rename(dir, str):
 def rename(dir, strA):
     # renames all subforders of dir, not including dir itself
     # dir = os.path.dirname(os.path.abspath(__file__))
-    def rename_all(root, items, index):
+
+    index = 0
+
+    def rename_all(root, items):
         for name in items:
             try:
+                global index
                 index += 1
                 os.rename(os.path.join(root, name),
                           os.path.join(root, strA + str(index) + str(os.path.splitext(os.path.join(root, name))[1].lower())))
             except OSError:
                 pass  # can't rename it, so what
-
-    index = 0
     for root, dirs, files in os.walk(dir, topdown=False):
         # rename_all(root, dirs)
-        rename_all(root, files, index)
- 
+        rename_all(root, files)
+
 
 # lst = []
 # index = 0
@@ -108,4 +107,5 @@ def rename(dir, strA):
 #             print(str(index))
 #             shutil.copyfile(os.path.join(root, file), os.path.join(r'\\192.168.31.127\New folder (2)\new\new', '000.00.38.H41.G07-LD11.' + str(index) + '.pdf'))
 
-rename(r'\\192.168.31.127\New folder (2)\new\so vhtt 000.00.46.H41\G16-VH14', '000.00.46.H41.' + 'G16-VH14.')
+rename(r'\\192.168.31.127\New folder (2)\new\so vhtt 000.00.46.H41\G16-VH14',
+       '000.00.46.H41.' + 'G16-VH14.')
--- Python/MySite/tutorial/urls.py
+++ Python/MySite/tutorial/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
      re_path(r'^$', views.index, name='index'),
      re_path(r'^index/', views.index, name='index'),
      re_path(r'^article/(\d+)/', views.viewArticle, name = 'article'), # Sending Parameters to Views
+     re_path(r'^hello/', views.hello, name='hello'),
 ]
