b88a2e3460bd6dccdfe0d63f2dd6ded5e52e4466
Nam Nguyễn <vannam.pxb@gmail.com>
2022-02-21 13:48:17 +0700

    Auto-committed on 13:48:16,54 - 21/02/2022

--- Python/MySite/MySite/settings.py
+++ Python/MySite/MySite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 # file cấu hình
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS  = [os.path.join(BASE_DIR, "static"), ]
+
+# STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
