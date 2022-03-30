2bbf1bd83b2b239b714e8fefdbb29310da527164
Nam Nguyễn <vannam.pxb@gmail.com>
2022-03-30 14:49:50 +0700

    Auto-committed on 14:49:50,31 - 30/03/2022

--- Python/MySite/tools/test.py
+++ Python/MySite/tools/test.py
@@ -3,10 +3,10 @@ import sys
 # stop Python from making .pyc files
 # sys.dont_write_bytecode = True
 
-x = 2000
-y = 36450
+x = 1000
+y = 36400
 
-result = (18819000 + x*y + (x*y)*0.01)/(500+x)
+result = (37019000 + x*y + (x*y)*0.01)/(1000+x)
 print(result)
 print(x*y)
 
