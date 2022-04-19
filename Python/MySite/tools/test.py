e4bbc6e90ebee4a0e4e6e6a99c2b0c00f2959ccb
Nam Nguyễn <vannam.pxb@gmail.com>
2022-04-19 08:30:47 +0700

    Auto-committed on  8:30:47,52 - 19/04/2022

--- Python/MySite/tools/test.py
+++ Python/MySite/tools/test.py
@@ -7,21 +7,21 @@ os.system('cmd /c "cls"')
 # stop Python from making .pyc files
 # sys.dont_write_bytecode = True
 
-x = 0
-y = 33200
+x = 500
+y = 30000
 
 result = (106005000 + x*y + (x*y)*0.01)/(3000+x)
 
 print("{:,}".format(round(result/1000, 2)))
 print("{:,}".format(round(int(result))))
-print("{:,}".format(x*y))
-print("------------------------------")
+print("{:,}".format(x*y + 0.01*x*y))
+# print("------------------------------")
 
-x = 0
-y = 15200
+# x = 0
+# y = 15200
 
-result = (10682700 + x*y + (x*y)*0.01)/(700+x)
+# result = (10682700 + x*y + (x*y)*0.01)/(700+x)
 
-print("{:,}".format(round(result/1000, 2)))
-print("{:,}".format(round(int(result))))
-print("{:,}".format(x*y))
+# print("{:,}".format(round(result/1000, 2)))
+# print("{:,}".format(round(int(result))))
+# print("{:,}".format(x*y))
