dc040af0208b5db995c793ec2b63ea2a8bddfe2f
namtao <vannam.pxb@gmail.com>
2022-07-15 09:41:27 +0700

    Auto-committed on  9:41:26,96 - 15/07/2022

--- Python/MySite/API/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from .models import Nguoidung
-
-class NguoidungSerilizer(serializers.ModelSerializer):
-    class Meta:
-        model = Nguoidung
-        fields = ['id','tendangnhap','matkhau','trangthai','ngaytao','ngaycapnhat','tendaydu','quyen']
--- Python/MySite/MyApp/form.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class RenameForm(forms.Form):
-    file_type = forms.CharField(max_length=1000)
-    root_path = forms.CharField(max_length=1000)
-    target_path = forms.CharField(max_length=1000)
-    filename = forms.CharField(max_length=1000)
