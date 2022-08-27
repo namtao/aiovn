506afec4c90b3ee7863d41cfa4cfcc98bdde04c0
namtao <vannam.pxb@gmail.com>
2022-08-27 15:02:17 +0700

    Auto-committed on 15:02:17,00 - 27/08/2022

---
+++ SQL/GiaoNhanThietBi.sql
@@ -0,0 +1,50 @@
+﻿declare @idThietBi INT;
+declare @txtNoiGui nvarchar(max) set @txtNoiGui = N'UBND TP Vị Thanh Tỉnh Hậu Giang'
+declare @txtNguoiGui nvarchar(max) set @txtNguoiGui = N'Đặng Minh Hoàng'
+declare @txtTTNguoiGui nvarchar(max) set @txtTTNguoiGui = N''
+declare @txtNoiNhan nvarchar(max) set @txtNoiNhan = N'Chi Nhánh miền Nam'
+declare @txtNguoiNhan nvarchar(max) set @txtNguoiNhan = N'Nguyễn Đức Quí'
+declare @txtTTNguoiNhan nvarchar(max) set @txtTTNguoiNhan = N''
+declare @txtNoiDung nvarchar(max) set @txtNoiDung = N'Di chuyển máy về chi nhánh'
+declare @txtNQL nvarchar(max) set @txtNQL = N'Chi Nhánh miền Nam'
+declare @txtBPQL nvarchar(max) set @txtBPQL = N'Phòng số hóa'
+declare @txtNgQL nvarchar(max) set @txtNgQL = N'Nguyễn Đức Quí'
+declare @txtGhiChu nvarchar(max) set @txtGhiChu = N''
+
+declare @txtDonVi nvarchar(max) set @txtDonVi = N'ADDJ'
+declare @txtTinhTrang INT set @txtTinhTrang = 1
+declare @txtTrangThai INT set @txtTrangThai = 1
+
+
+DECLARE cursorQuery CURSOR FOR
+
+select id from ThietBi where id in (514,515,516, 517)
+
+OPEN cursorQuery               
+
+FETCH NEXT FROM cursorQuery    
+INTO @idThietBi
+
+WHILE @@FETCH_STATUS = 0         
+BEGIN
+                                 
+                                 
+    insert into LichSuThietBi 
+	values(@idThietBi, CONVERT(nvarchar(10), '12/08/2022', 103), @txtNoiGui, @txtNguoiGui, 
+	@txtTTNguoiGui,  @txtNoiNhan,  @txtNguoiNhan, @txtTTNguoiNhan,  @txtNoiDung,  @txtNQL,  @txtBPQL,  @txtNgQL, @txtGhiChu )
+
+	update ThietBi 
+	set DonVi = @txtDonVi, 
+	NoiQuanLy = @txtNQL, 
+	BoPhanQuanLy = @txtBPQL, 
+	NguoiQuanLy = @txtNgQL, 
+	TinhTrang = @txtTinhTrang, 
+	TrangThai = @txtTrangThai 
+	where id = @idThietBi 
+
+    FETCH NEXT FROM cursorQuery 
+	INTO @idThietBi
+END
+
+CLOSE cursorQuery             
+DEALLOCATE cursorQuery
