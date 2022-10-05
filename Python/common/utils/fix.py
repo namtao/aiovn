24dcf1ca9261633f84fa2a04d6f2623043aeae14
namtao <vannam.pxb@gmail.com>
2022-10-05 10:27:49 +0700

    Auto-committed on 10:27:49,14 - 05/10/2022

--- Python/common/utils/fix.py
+++ Python/common/utils/fix.py
@@ -2,27 +2,29 @@ import os
 import re
 import shutil
 
+# lấy tên file => xóa data => xóa ảnh => up lại
+
 # di chuyển ảnh về thư mục lỗi
-# lst = []
-# for root, dirs, files in os.walk(r'D:\HoTich\HOTICH_HG\source - haugiang\Files'):
-#         for file in files:    
-#             with open('error.txt', 'r') as f:           
-#                 for line in f:
-#                     if line.strip() == file:
-#                         # print (os.path.join(root, file))
-#                         shutil.move(os.path.join(root, file), os.path.join('D:\Error', file))
+lst = []
+for root, dirs, files in os.walk(r'D:\HoTich\HOTICH_HG\source - haugiang\Files'):
+        for file in files:    
+            with open('error.txt', 'r') as f:           
+                for line in f:
+                    if line.strip() == file:
+                        # print (os.path.join(root, file))
+                        shutil.move(os.path.join(root, file), os.path.join('E:\Error', file))
                    
             # while True:
             #     line = f.readline()
             #     if line.strip() == file:
             #         print (os.path.join(root, file))
-            #         shutil.move(os.path.join(root, file), os.path.join('D:\Error', file))
+            #         shutil.move(os.path.join(root, file), os.path.join('E:\Error', file))
             #         break
             
 
 # ghi file txt lỗi
 # with open('error.txt', 'w+') as f:               
-#     for root, dirs, files in os.walk(r'E:\Sua loi\Da sua'):
+#     for root, dirs, files in os.walk(r'E:\Sua loi\Lan 2\goc'):
 #         for file in files:
 #             pattern = re.compile(".*"+'pdf'+"$")
 
@@ -30,13 +32,13 @@ import shutil
 #                 f.write(file + '\n')
 
 
-lst = []
-for root, dirs, files in os.walk(r'D:\HoTich\HOTICH_HG\source - haugiang\Files'):
-    for file in files:
-        pattern = re.compile(".*"+'pdf'+"$")
+# lst = []
+# for root, dirs, files in os.walk(r'D:\HoTich\HOTICH_HG\source - haugiang\Files'):
+#     for file in files:
+#         pattern = re.compile(".*"+'pdf'+"$")
 
-        if pattern.match(file):
-            # lst.append(os.path.join(root, file))
-            lst.append(os.path.join(file))
-            # lst.append(os.path.splitext(file)[0])
+#         if pattern.match(file):
+#             # lst.append(os.path.join(root, file))
+#             lst.append(os.path.join(file))
+#             # lst.append(os.path.splitext(file)[0])
                     
