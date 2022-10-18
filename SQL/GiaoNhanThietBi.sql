a878bcc06953a3c1d63273f0521c032aa677d085
namtao <vannam.pxb@gmail.com>
2022-10-18 17:22:55 +0700

    Auto-committed on 17:22:55,63 - 18/10/2022

--- SQL/GiaoNhanThietBi.sql
+++ SQL/GiaoNhanThietBi.sql
@@ -1,14 +1,14 @@
 ﻿declare @idThietBi INT;
 declare @txtNoiGui nvarchar(max) set @txtNoiGui = N'UBND TP Vị Thanh Tỉnh Hậu Giang'
-declare @txtNguoiGui nvarchar(max) set @txtNguoiGui = N'Đặng Minh Hoàng'
+declare @txtNguoiGui nvarchar(max) set @txtNguoiGui = N'Phạm Ngọc Thịnh'
 declare @txtTTNguoiGui nvarchar(max) set @txtTTNguoiGui = N''
-declare @txtNoiNhan nvarchar(max) set @txtNoiNhan = N'Chi Nhánh miền Nam'
-declare @txtNguoiNhan nvarchar(max) set @txtNguoiNhan = N'Nguyễn Đức Quí'
+declare @txtNoiNhan nvarchar(max) set @txtNoiNhan = N'Sở Tư Pháp Đắk Nông'
+declare @txtNguoiNhan nvarchar(max) set @txtNguoiNhan = N'Phạm Ngọc Thịnh'
 declare @txtTTNguoiNhan nvarchar(max) set @txtTTNguoiNhan = N''
-declare @txtNoiDung nvarchar(max) set @txtNoiDung = N'Di chuyển máy về chi nhánh'
-declare @txtNQL nvarchar(max) set @txtNQL = N'Chi Nhánh miền Nam'
+declare @txtNoiDung nvarchar(max) set @txtNoiDung = N'Di chuyển máy đi dự án'
+declare @txtNQL nvarchar(max) set @txtNQL = N'Sở Tư Pháp Đắk Nông'
 declare @txtBPQL nvarchar(max) set @txtBPQL = N'Phòng số hóa'
-declare @txtNgQL nvarchar(max) set @txtNgQL = N'Nguyễn Đức Quí'
+declare @txtNgQL nvarchar(max) set @txtNgQL = N''
 declare @txtGhiChu nvarchar(max) set @txtGhiChu = N''
 
 declare @txtDonVi nvarchar(max) set @txtDonVi = N'ADDJ'
@@ -18,7 +18,8 @@ declare @txtTrangThai INT set @txtTrangThai = 1
 
 DECLARE cursorQuery CURSOR FOR
 
-select id from ThietBi where id in (514,515,516, 517)
+select id from ThietBi where id in (618,620,621)
+
 
 OPEN cursorQuery               
 
@@ -30,7 +31,7 @@ BEGIN
                                  
                                  
     insert into LichSuThietBi 
-	values(@idThietBi, CONVERT(nvarchar(10), '12/08/2022', 103), @txtNoiGui, @txtNguoiGui, 
+	values(@idThietBi, CONVERT(nvarchar(10), '15/08/2022', 103), @txtNoiGui, @txtNguoiGui, 
 	@txtTTNguoiGui,  @txtNoiNhan,  @txtNguoiNhan, @txtTTNguoiNhan,  @txtNoiDung,  @txtNQL,  @txtBPQL,  @txtNgQL, @txtGhiChu )
 
 	update ThietBi 
