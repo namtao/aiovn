7dd84466d118da811010777eb710804b8ee5c534
namtao <vannam.pxb@gmail.com>
2022-11-08 06:56:17 +0700

    Auto-committed on  6:56:16,83 - 08/11/2022

--- remove_temp.py
+++ remove_temp.py
@@ -5,17 +5,17 @@ import shutil
 
 from PyPDF2 import PdfFileMerger, PdfFileReader, PdfFileWriter
 
-path = r'E:\OCR NEN\Nen\OCR NEN\Chua nen\duoi 2006'
+path = r'E:\OCR NEN\Chua nen\duoi 2006'
 
 for root, dirs, files in os.walk(path):
     for dir in dirs:
-        if(len(dir) > 10 or ('Temp' in dir)):
+        if (len(dir) > 10 or ('Temp' in dir)):
             print(os.path.join(root, dir))
             shutil.rmtree(os.path.join(root, dir))
 
 for root, dirs, files in os.walk(path):
     for file in files:
-        if(('_' in file) or os.path.getsize(os.path.join(root, file)) == 0):
+        if (('_' in file) or os.path.getsize(os.path.join(root, file)) == 0):
             print(os.path.join(root, file))
             os.remove(os.path.join(root, file))
 
@@ -46,7 +46,8 @@ def split_pdf():
             pattern = re.compile(".*"+'pdf'+"$")
 
             if pattern.match(file):
-                inputpdf = PdfFileReader(os.path.join(root, file), strict=False)
+                inputpdf = PdfFileReader(
+                    os.path.join(root, file), strict=False)
                 fileName = pathlib.Path(file).stem
                 parentPath = pathlib.Path(file).parent.absolute()
 
@@ -57,29 +58,36 @@ def split_pdf():
                     output.addPage(inputpdf.getPage(i))
                     with open("%s-%s.pdf" % (os.path.join(r'E:\OCR NEN\Chua nen\2006-2015 - Copy', fileName), i), "wb") as outputStream:
                         output.write(outputStream)
-                        pdfs.append("%s-%s.pdf" % (os.path.join(r'E:\OCR NEN\Chua nen\2006-2015 - Copy', fileName), i))
+                        pdfs.append(
+                            "%s-%s.pdf" % (os.path.join(r'E:\OCR NEN\Chua nen\2006-2015 - Copy', fileName), i))
                         outputStream.close()
-                        
+
+
 def merge_pdf():
     for root, dirs, files in os.walk(r'E:\OCR NEN\Nen chua ghep'):
         for file in files:
             pattern = re.compile(".*pdf$")
 
             if pattern.match(file):
-                inputpdf = PdfFileReader(os.path.join(root, file), strict=False)
+                inputpdf = PdfFileReader(
+                    os.path.join(root, file), strict=False)
                 fileName = pathlib.Path(file).stem
                 parentPath = pathlib.Path(file).parent.absolute()
-                
+
                 if not os.path.exists(os.path.join(r'E:\OCR NEN\Nen ghep', file.split('-')[0] + '.pdf')):
                     merger = PdfFileMerger()
                     merger.append(os.path.join(root, file))
-                    merger.write(os.path.join(r'E:\OCR NEN\Nen ghep', file.split('-')[0] + '.pdf'))
+                    merger.write(os.path.join(
+                        r'E:\OCR NEN\Nen ghep', file.split('-')[0] + '.pdf'))
                 else:
                     merger = PdfFileMerger()
-                    merger.append(PdfFileReader(os.path.join(r'E:\OCR NEN\Nen ghep', file.split('-')[0] + '.pdf'), 'rb'))  # type: ignore
-                    merger.append(PdfFileReader(os.path.join(root, file), 'rb'))  # type: ignore
-                    merger.write(os.path.join(r'E:\OCR NEN\Nen ghep', file.split('-')[0] + '.pdf'))
-                                        
+                    merger.append(PdfFileReader(os.path.join(
+                        r'E:\OCR NEN\Nen ghep', file.split('-')[0] + '.pdf'), 'rb'))  # type: ignore
+                    merger.append(PdfFileReader(os.path.join(
+                        root, file), 'rb'))  # type: ignore
+                    merger.write(os.path.join(
+                        r'E:\OCR NEN\Nen ghep', file.split('-')[0] + '.pdf'))
 
-# merge_pdf()  
-# removeEmptyFolders(path)
+
+# merge_pdf()
+# removeEmptyFolders(r'D:\HoTich\HOTICH_HG\source - vithuy\Files\Trung')
