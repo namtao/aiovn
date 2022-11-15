6b2b7b7a8fa36353608635736c5d7317c2ce4e96
namtao <vannam.pxb@gmail.com>
2022-11-15 11:10:18 +0700

    Auto-committed on 11:10:17,83 - 15/11/2022

--- other/remove_temp.py
+++ other/remove_temp.py
@@ -1,95 +1,109 @@
-import logging
 import os
-import pathlib
 import re
 import shutil
 
-from PyPDF2 import PdfFileMerger, PdfFileReader, PdfFileWriter
 
-path = r'E:\OCR NEN - LONG MY\CHUA NEN'
-logging.basicConfig(level=logging.DEBUG)
+# xóa file rác và thư mục trống
+def remove_temp(path):
+    for root, dirs, files in os.walk(path):
+        for dir in dirs:
+            if (len(dir) > 10 or ('Temp' in dir)):
+                print(os.path.join(root, dir))
+                shutil.rmtree(os.path.join(root, dir))
 
-for root, dirs, files in os.walk(path):
-    for dir in dirs:
-        if (len(dir) > 10 or ('Temp' in dir)):
-            logging.info(os.path.join(root, dir))
-            shutil.rmtree(os.path.join(root, dir))
-
-for root, dirs, files in os.walk(path):
-    for file in files:
-        if (('_' in file) or os.path.getsize(os.path.join(root, file)) == 0 or ('jpg' in file)):
-            logging.info(os.path.join(root, file))
-            os.remove(os.path.join(root, file))
-
-
-def removeEmptyFolders(path, removeRoot=True):
-    'Function to remove empty folders'
-    if not os.path.isdir(path):
-        return
-
-    # remove empty subfolders
-    files = os.listdir(path)
-    if len(files):
-        for f in files:
-            fullpath = os.path.join(path, f)
-            if os.path.isdir(fullpath):
-                removeEmptyFolders(fullpath)
-
-    # if folder empty, delete it
-    files = os.listdir(path)
-    if len(files) == 0 and removeRoot:
-        print("Removing empty folder:", path)
-        os.rmdir(path)
-
-
-def split_pdf():
-    for root, dirs, files in os.walk(r'E:\OCR NEN\Chua nen\2006-2015 - Copy'):
+    for root, dirs, files in os.walk(path):
         for file in files:
-            pattern = re.compile(".*"+'pdf'+"$")
+            if (('_' in file) or os.path.getsize(os.path.join(root, file)) == 0 or ('jpg' in file)):
+                print(os.path.join(root, file))
+                os.remove(os.path.join(root, file))
 
-            if pattern.match(file):
-                inputpdf = PdfFileReader(
-                    os.path.join(root, file), strict=False)
-                fileName = pathlib.Path(file).stem
-                parentPath = pathlib.Path(file).parent.absolute()
+    def removeEmptyFolders(path, removeRoot=True):
+        'Function to remove empty folders'
+        if not os.path.isdir(path):
+            return
 
-                pdfs = []
+        # remove empty subfolders
+        files = os.listdir(path)
+        if len(files):
+            for f in files:
+                fullpath = os.path.join(path, f)
+                if os.path.isdir(fullpath):
+                    removeEmptyFolders(fullpath)
 
-                for i in range(inputpdf.numPages):
-                    output = PdfFileWriter()
-                    output.addPage(inputpdf.getPage(i))
-                    with open("%s-%s.pdf" % (os.path.join(r'E:\OCR NEN\Chua nen\2006-2015 - Copy', fileName), i), "wb") as outputStream:
-                        output.write(outputStream)
-                        pdfs.append(
-                            "%s-%s.pdf" % (os.path.join(r'E:\OCR NEN\Chua nen\2006-2015 - Copy', fileName), i))
-                        outputStream.close()
+        # if folder empty, delete it
+        files = os.listdir(path)
+        if len(files) == 0 and removeRoot:
+            print("Removing empty folder:", path)
+            os.rmdir(path)
+
+    removeEmptyFolders(path)
 
 
-def merge_pdf():
-    for root, dirs, files in os.walk(r'E:\OCR NEN\Nen chua ghep'):
+# kiểm tra file chưa nén và file nén xem đã đầy đủ chưa
+def copy_nen(dir1, dir2, pathTarget):
+    def get_files(folderPath, fileFormat):
+        lst = []
+        for root, dirs, files in os.walk(folderPath):
+            for file in files:
+                pattern = re.compile(".*"+fileFormat+"$")
+
+                if pattern.match(file):
+                    # lst.append(os.path.join(root, file))
+                    lst.append(os.path.join(file))
+                    # lst.append(os.path.splitext(file)[0])
+        return lst
+
+    lstPdf1 = get_files(dir1, 'pdf')
+    lstPdf2 = get_files(dir2, 'pdf')
+    # lstPdf3 = get_files(dir3, 'pdf')
+
+    # lstDuplicate = list(set(lstPdf1) & set(lstPdf2))
+    lstNotDuplicate = list(set(lstPdf1) - set(lstPdf2))
+
+    # print(len(lstDuplicate))
+
+    # count = 0
+    for root, dirs, files in os.walk(dir1):
         for file in files:
-            pattern = re.compile(".*pdf$")
-
-            if pattern.match(file):
-                inputpdf = PdfFileReader(
-                    os.path.join(root, file), strict=False)
-                fileName = pathlib.Path(file).stem
-                parentPath = pathlib.Path(file).parent.absolute()
-
-                if not os.path.exists(os.path.join(r'E:\OCR NEN\Nen ghep', file.split('-')[0] + '.pdf')):
-                    merger = PdfFileMerger()
-                    merger.append(os.path.join(root, file))
-                    merger.write(os.path.join(
-                        r'E:\OCR NEN\Nen ghep', file.split('-')[0] + '.pdf'))
-                else:
-                    merger = PdfFileMerger()
-                    merger.append(PdfFileReader(os.path.join(
-                        r'E:\OCR NEN\Nen ghep', file.split('-')[0] + '.pdf'), 'rb'))
-                    merger.append(PdfFileReader(os.path.join(
-                        root, file), 'rb'))
-                    merger.write(os.path.join(
-                        r'E:\OCR NEN\Nen ghep', file.split('-')[0] + '.pdf'))
+            if (file in lstNotDuplicate):
+                print(os.path.join(root, file))
+                shutil.copy(os.path.join(root, file),
+                            os.path.join(pathTarget, file))
 
 
-# merge_pdf()
-removeEmptyFolders(path)
+def remove_nen(dir1, dir2):
+    def get_files(folderPath, fileFormat):
+        lst = []
+        for root, dirs, files in os.walk(folderPath):
+            for file in files:
+                pattern = re.compile(".*"+fileFormat+"$")
+
+                if pattern.match(file):
+                    # lst.append(os.path.join(root, file))
+                    lst.append(os.path.join(file))
+                    # lst.append(os.path.splitext(file)[0])
+        return lst
+
+    # dir3 = r'E:\Chua nen'
+
+    lstPdf1 = get_files(dir1, 'pdf')
+    lstPdf2 = get_files(dir2, 'pdf')
+    # lstPdf3 = get_files(dir3, 'pdf')
+
+    lstDuplicate = list(set(lstPdf1) & set(lstPdf2))
+    # lstNotDuplicate = list(set(lstPdf1) - set(lstPdf2) - set(lstPdf3))
+
+    # print(len(lstDuplicate))
+
+    for root, dirs, files in os.walk(dir1):
+        for file in files:
+            if (file in lstDuplicate):
+                print(os.path.join(root, file))
+                # os.remove(os.path.join(root, file))
+
+
+remove_temp(r'E:\OCR NEN\CHUA NEN')
+
+# copy_nen(r'D:\HoTich\HOTICH_HG\source - vithuy\Files', r'D:\HoTich\HOTICH_HG\source - vithuy\FilesNen', r'E:\OCR NEN\CHUA NEN\VI THUY')
+
+remove_nen(r'E:\OCR NEN\CHUA NEN', r'E:\OCR NEN\NEN')
