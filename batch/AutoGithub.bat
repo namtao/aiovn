03888e9c1908da52f2c9412363ca7fd845701d48
namtao <vannam.pxb@gmail.com>
2022-11-22 14:18:20 +0700

    Auto-committed on 14:18:20,82 - 22/11/2022

--- batch/AutoGithub.bat
+++ batch/AutoGithub.bat
@@ -1,6 +1,8 @@
 ECHO OFF
 :: Di chuyển đến thư mục chứa code của chúng ta
 :: cd "C:xD"
+:: cập nhật thư viện
+python -m pip freeze --all > requirements.txt
 :: Set các giá trị về file git exe và nhánh cần làm việc
 set GIT_PATH="C:\Program Files\Git\bin\git.exe"
 set BRANCH = "origin"
