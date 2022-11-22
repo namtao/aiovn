bea39fa887a531dc272a74faf2ca8f62655e506e
namtao <vannam.pxb@gmail.com>
2022-11-22 14:29:47 +0700

    Auto-committed on 14:29:47,08 - 22/11/2022

--- batch/AutoCommit.bat
+++ batch/AutoCommit.bat
@@ -1,17 +1,23 @@
 ECHO OFF
 :: Di chuyển đến thư mục chứa code của chúng ta
 :: cd "C:xD"
+
 :: cập nhật thư viện
 cd ..
-python -m pip freeze --all > requirements.txt
+env\Scripts\python.exe -m pip freeze --all > requirements.txt
+
 :: Set các giá trị về file git exe và nhánh cần làm việc
 set GIT_PATH="C:\Program Files\Git\bin\git.exe"
 set BRANCH = "origin"
+
 :: Tiến hành pull toàn bộ code về
 %GIT_PATH% pull %BRANCH%
+
 :: Tiến hành add toàn bộ file thay đổi
 %GIT_PATH% add -A
-:: Commit lên
+
+:: Commit
 %GIT_PATH% commit -am "Auto-committed on %time% - %date%"
-:: Push lên
+
+:: Push
 %GIT_PATH% push %BRANCH%
