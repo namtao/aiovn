b96c99223c022a5834ba010610d0c5a6a504b245
namtao <vannam.pxb@gmail.com>
2022-12-13 16:41:17 +0700

    Auto-committed on 16:41:17,76 - 13/12/2022

--- api/main.py
+++ api/main.py
@@ -8,15 +8,5 @@ app = FastAPI()
 async def root():
     return {"message": "Hello world"}
 
-
-app = FastAPI()
-
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]  # pair format: key-value
-
-
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 1):
-    return fake_items_db[skip: skip + limit]  # trả về dữ liệu từ skip đến skip + limit
-
 if __name__ == '__main__':
     uvicorn.run(app, port=5000, reload=True)
