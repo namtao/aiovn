468e442935532da2a264e7cc2abc73cb8fa13fab
namtao <vannam.pxb@gmail.com>
2022-12-15 14:23:23 +0700

    Auto-committed on 14:23:23,21 - 15/12/2022

--- api/main.py
+++ api/main.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 
 app = FastAPI()
 
@@ -13,6 +13,12 @@ app = FastAPI()
 
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]  # pair format: key-value
 
+@app.get("/test")
+async def get_json():
+
+    test = "[1592494390, 'test', -0.2761097089544078, -0.0852381808812182, -0.101153, nan]"
+    # print(test)
+    return Response(content=test, media_type="application/json")
 
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 1):
