22374498140a1544af0fe32b0fc3b7793d5e65e7
namtao <vannam.pxb@gmail.com>
2022-12-20 19:56:05 +0700

    Auto-committed on 19:56:05,25 - 20/12/2022

---
+++ loading.py
@@ -0,0 +1,45 @@
+import configparser
+import itertools
+import os
+import time
+import urllib.parse
+from itertools import cycle
+from sys import stdout as terminal
+from threading import Thread
+from time import sleep
+
+import pandas as pd
+
+done = False
+def clear(): 
+    return os.system('cls')
+
+
+def animate():
+    for c in itertools.cycle(['⠷', '⠯', '⠟', '⠻', '⠽', '⠾']):
+        if done:
+            break
+        terminal.write('\rLoading ' + c + ' ')
+        terminal.flush()
+        sleep(0.1)
+    # terminal.write('\rDone!    ')
+    terminal.flush()
+
+
+t = Thread(target=animate)
+t.start()
+
+
+# Chay lenh tai day
+config = configparser.ConfigParser()
+config.read(r'config.ini')
+conn = f'mssql://{config["local"]["user"]}:{urllib.parse.quote_plus(config["local"]["pass"])}@{config["local"]["host"]}/{config["local"]["db"]}?driver={config["local"]["driver"]}'
+
+
+start_time = time.time()
+
+pd.read_sql('select * from ht_khaisinh', conn)
+# clear()
+print("\rPandas finished --- %s seconds ---" % (time.time() - start_time))
+
+done = True
