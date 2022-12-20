aac3cceb2d39b8be3b37a5bfb57727de79a4aa72
namtao <vannam.pxb@gmail.com>
2022-12-20 18:38:46 +0700

    Auto-committed on 18:38:46,52 - 20/12/2022

--- python/pandasx.py
+++ python/pandasx.py
@@ -3,28 +3,15 @@ import configparser
 import time
 import urllib.parse
 
-import connectorx as cx
-import dask.dataframe as dd
 import pandas as pd
 
 config = configparser.ConfigParser()
 config.read(r'config.ini')
 conn = f'mssql://{config["local"]["user"]}:{urllib.parse.quote_plus(config["local"]["pass"])}@{config["local"]["host"]}/{config["local"]["db"]}?driver={config["local"]["driver"]}'
 
-conntest = f'mssql://{config["localtest"]["user"]}:{urllib.parse.quote_plus(config["localtest"]["pass"])}@{config["localtest"]["host"]}/{config["localtest"]["db"]}'
 
+start_time = time.time()
 
-# start_time = time.time()
+pd.read_sql('select * from KS', conn)
 
-# pd.read_sql_query('select * from DC_DMTINHTRANG', conn)
-
-# print("Pandas finished --- %s seconds ---" % (time.time() - start_time))
-
-# start_time = time.time()
-
-# dd.read_sql_query("SELECT * FROM DC_DMTINHTRANG", conn, index_col='TinhTrangID', npartitions=1)
-
-# print("Dask finished --- %s seconds ---" % (time.time() - start_time))
-
-
-print(cx.read_sql(conntest, 'select * from KS'))
+print("Pandas finished --- %s seconds ---" % (time.time() - start_time))
