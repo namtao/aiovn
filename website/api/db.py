5dff49b39e0abbbed7290b7b2a9560d4ed6bbde3
namtao <vannam.pxb@gmail.com>
2022-12-30 15:15:46 +0700

    Auto-committed on 15:15:45,90 - 30/12/2022

--- website/api/db.py
+++ website/api/db.py
@@ -4,11 +4,15 @@ import json
 import urllib.parse
 
 import pandas as pd
-from fastapi import APIRouter, FastAPI, Request, Response
+import pyodbc
+from fastapi import APIRouter, Depends, FastAPI, Request, Response
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.security import APIKeyCookie
 from fastapi.templating import Jinja2Templates
+from models.hotich import *
+from sqlalchemy import MetaData, create_engine, update
+from sqlalchemy.orm import Session, sessionmaker
 
 router = APIRouter()
 cookie_sec = APIKeyCookie(name="session")
@@ -18,3 +22,36 @@ config = configparser.ConfigParser()
 
 config.read(r'D:\xD\config.ini')
 conn = f'mssql://{config["hn"]["user"]}:{urllib.parse.quote_plus(config["hn"]["pass"])}@{config["hn"]["host"]}/{config["hn"]["db"]}?driver={config["hn"]["driver"]}'
+
+
+engine = create_engine(
+    "mssql+pyodbc://sa:Addj%40123@WIN-QDNNB992THP\SQLEXPRESS/hotichdata?driver=ODBC+Driver+17+for+SQL+Server")
+
+meta = MetaData()
+
+conn = engine.connect()
+
+
+def get_db():
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    db: Session = SessionLocal()
+    try:
+        yield db
+        db.commit()
+    except Exception:
+        db.rollback()
+    finally:
+        db.close()
+
+@router.get("/update")
+async def GetAll(db: Session = Depends(get_db)):
+
+    # get the model.patient with the given id
+    # using sql alchemy orm, we're querying the Patient table
+    query = db.query(t_Diff).filter_by(id=164407).update({"so":""})
+    patients = db.query(t_Diff).filter_by(id=164407).all()
+    
+    # upd = update(t_Diff)
+    # val = upd.values({"so": "value"})
+    # cond = val.where(t_Diff.c.column_name == value)
+    return patients
