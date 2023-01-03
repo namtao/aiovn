03e67c0930dbe85e23000575d703a644b607786f
namtao <vannam.pxb@gmail.com>
2023-01-03 11:21:02 +0700

    Auto-committed on 11:21:01,54 - 03/01/2023

--- Dockerfile
+++ Dockerfile
@@ -15,5 +15,5 @@ RUN pip install -r /app/requirements.txt
 # COPY ./app /code/app
 
 # 
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["uvicorn", "main:app", "--host", "127.0.0.1", "--port", "8000"]
 # CMD [ "python", "test.py" ]
--- other/tim_khac_nhau.py
+++ other/tim_khac_nhau.py
@@ -14,7 +14,7 @@ with open(r'other\lst2.txt', 'r') as fr:
         lst2 = fr.read().splitlines()
 
 # lstDuplicate = list(set(lstPdf1) & set(lstPdf2))
-lstNotDuplicate = list(set(lst1) - set(lst2))
+lstNotDuplicate = list(set(lst2) - set(lst1))
 
 with open(r'other\ketqua.txt', 'w+') as fw:
 
