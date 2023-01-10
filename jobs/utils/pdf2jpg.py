c3a2c61878711eaccec25c289098ceb1e9859d3d
namtao <vannam.pxb@gmail.com>
2023-01-10 09:42:23 +0700

    Auto-committed on  9:42:22,67 - 10/01/2023

--- jobs/utils/pdf2jpg.py
+++ jobs/utils/pdf2jpg.py
@@ -8,7 +8,7 @@ from pdf2image import convert_from_path
 def pdf2jpg(pdfPath, jpgPath):
     pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
     pages = convert_from_path(
-        pdfPath, 500, poppler_path=r'MySite\library\poppler\bin')
+        pdfPath, 500, poppler_path=r'library\poppler-22.12.0\Library\bin')
     image_counter = 1
     for page in pages:
         filename = "page_"+str(image_counter)+".jpg"
@@ -18,7 +18,7 @@ def pdf2jpg(pdfPath, jpgPath):
 
 if __name__ == "__main__":
     pdfPath = r'C:\Users\Administrator\Downloads\test\KH.1964.93024.01.A4.64.54.pdf'
-    jpgPath = r'C:\Users\Administrator\Downloads\test\KH.1964.93024.01.A4.64.54.jpg'
+    jpgPath = r'C:\Users\Administrator\Downloads\test'
     pdf2jpg(pdfPath, jpgPath)
     
     
