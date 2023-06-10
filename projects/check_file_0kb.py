1f5f0083759a49561eb8f0867e8608472dec511f
namtao <vannam.pxb@gmail.com>
2023-06-10 11:02:34 +0700

    delete backend views

--- projects/check_file_0kb.py
+++
@@ -1,6 +0,0 @@
-import os
-
-for root, dirs, files in os.walk(r'D:\HoTich\HOTICH_HG\soucre - hlongmy\Files'):
-    for file in files:
-        if (os.path.getsize(os.path.join(root, file)) == 0):
-            print(os.path.join(root, file))
