41be7c809314a648b9baa413353b0972a8b0c8fd
namtao <vannam.pxb@gmail.com>
2024-04-01 19:58:41 +0700

    poetry

--- factorial_namtao/factorial_namtao/factorial.py
+++
@@ -1,7 +0,0 @@
-def fact(n):
-    if n < 0:
-        raise ValueError('Factorial is not defined for negative numvers')
-    elif n ==0 or n == 1:
-        return 1
-    else:
-        return n * fact(n-1)    
